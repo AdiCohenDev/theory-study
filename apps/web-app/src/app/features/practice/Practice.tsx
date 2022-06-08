966c63957cde99db0ae0b95205e2b51867b15d83
Adi Cohen <adicohencc@gmail.com>
2022-06-08 11:36:13 +0300

    pactice logic - save and fetch questions

--- apps/web-app/src/app/features/practice/Practice.tsx
+++ apps/web-app/src/app/features/practice/Practice.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import AnswerBox from '../../shared/components/AnswerBox/AnswerBox';
 import './Practice.css';
+import { fetchQuestionForUser } from './questions';
 
 interface CurrentQuestion {
   question: string;
@@ -14,14 +15,20 @@ interface Answer {
   isCorrect: boolean;
 }
 
-type UserAnswers = Record<number, number>;
+interface userAnswer {
+  answerId?: number;
+  expDate?: string;
+  never?: boolean;
+}
+
+export type UserAnswers = Record<number, userAnswer>;
 
 const Practice = () => {
   const [currentQuestion, setCurrentQuestion] = useState<CurrentQuestion>(null!);
   const [questionNum, setQuestionNum] = useState(0);
   const [userAnswers, setUserAnswers] = useState<UserAnswers>({});
   const [showCorrectAnswer, setShowCorrectAnswer] = useState<boolean>(false);
-  const [userAnswerId, setUserAnswerId] = useState<number>(null!);
+  const [userAnswerId, setUserAnswerId] = useState<number | undefined>(null!);
 
   const questions = [
     {
@@ -58,31 +65,77 @@ const Practice = () => {
 
   useEffect(() => {
     setCurrentQuestion(questions[questionNum]);
+    fetchQuestionForUser();
   }, [questionNum]);
 
   const saveUserProgress = (event: any) => {
-    console.log(event.id);
-    userAnswers[currentQuestion.num] = event.id;
+    userAnswers[currentQuestion.num] = {};
+    userAnswers[currentQuestion.num].answerId = event.id;
+    const userAnswerId = userAnswers[currentQuestion.num].answerId;
+    setUserAnswerId(userAnswerId);
 
     setUserAnswers({
       ...userAnswers,
     });
-    console.log(userAnswers);
+    setShowCorrectAnswer(true);
   };
   const prevQuestion = () => {
     if (questionNum > 0) {
       setQuestionNum(questionNum - 1);
+    }
+    if (showCorrectAnswer) {
+      setShowCorrectAnswer(false);
     }
   };
   const nextQuestion = () => {
     if (questionNum < questions.length - 1) {
       setQuestionNum(questionNum + 1);
     }
+    if (showCorrectAnswer) {
+      setShowCorrectAnswer(false);
+    }
   };
   const correctAnswerReveal = () => {
     setShowCorrectAnswer(true);
-    const userAnswerId = userAnswers[currentQuestion.num];
+    const userAnswerId = userAnswers[currentQuestion.num].answerId;
     setUserAnswerId(userAnswerId);
+  };
+
+  const easy = 'קל';
+  const middle = 'בינוני';
+  const hard = 'קשה';
+  const hide = 'תסתיר שאלה זו';
+
+  const questionLevel = (e: any) => {
+    const userChoice = e.target.innerText;
+    let seconds = 60;
+    const _2MinuteInSeconds = 120;
+    const _1HourInSeconds = 3600;
+    const _7DaysInSeconds = 604800;
+    let never = false;
+    if (userChoice === easy) {
+      seconds = _7DaysInSeconds;
+    }
+    if (userChoice === middle) {
+      seconds = _1HourInSeconds;
+    }
+    if (userChoice === hard) {
+      seconds = _2MinuteInSeconds;
+    }
+    if (userChoice === hide) {
+      never = true;
+    }
+    if (never) {
+      userAnswers[currentQuestion.num].never = true;
+    } else {
+      const expDate = new Date(new Date().getTime() + seconds * 1000).toISOString();
+      userAnswers[currentQuestion.num].expDate = expDate;
+    }
+    nextQuestion();
+  };
+
+  const finishPractice = () => {
+    //update the db
   };
   return (
     <div className="practice-container">
@@ -103,19 +156,33 @@ const Practice = () => {
         })}
       </div>
       <div className="navigate-questions">
-        <span onClick={() => prevQuestion()}>לשאלה הקודמת</span>
-        <span onClick={() => nextQuestion()}>לשאלה הבאה</span>
+        <span onClick={() => prevQuestion()} className="question-navigation">
+          לשאלה הקודמת
+        </span>
+        <span onClick={() => nextQuestion()} className="question-navigation">
+          לשאלה הבאה
+        </span>
       </div>
       <button className="show-right-answer-btn" onClick={correctAnswerReveal}>
         הצג תשובה נכונה
       </button>
       <div>
-        <button className="level-btn">תסתיר שאלה זו</button>
-        <button className="level-btn">קל</button>
-        <button className="level-btn">בינוני</button>
-        <button className="level-btn">קשה</button>
+        <button className="level-btn" onClick={questionLevel}>
+          {hide}
+        </button>
+        <button className="level-btn" onClick={questionLevel}>
+          {easy}
+        </button>
+        <button className="level-btn" onClick={questionLevel}>
+          {middle}
+        </button>
+        <button className="level-btn" onClick={questionLevel}>
+          {hard}
+        </button>
       </div>
-      <button className="finish-practice-btn">סיים תרגול</button>
+      <button className="finish-practice-btn" onClick={finishPractice}>
+        סיים תרגול
+      </button>
     </div>
   );
 };
