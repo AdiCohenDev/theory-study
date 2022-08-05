39dc4dda9b022aa6ef9a4c646452bdb6cba1816f
Adi Cohen <adicohencc@gmail.com>
2022-08-05 14:40:10 +0300

    improving home page

--- apps/web-app/src/app/features/auth/components/home/Home.tsx
+++ apps/web-app/src/app/features/auth/components/home/Home.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ProgressPie from '../../../progress-pie/progressPie';
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <>
+      <div className="home-container">
+        <div className="home-title">בואו נלמד לתיאוריה</div>
+        <div>
+          <div className="home-practice-title">שנתרגל?</div>
+          <button onClick={() => navigate('/practice')}>לתרגול השאלות</button>
+        </div>
+        {/*<div>*/}
+        {/*  <div>שננסה להיבחן?</div>*/}
+        {/*  <button>קח אותי למבחן!</button>*/}
+        {/*</div>*/}
+      </div>
+      <hr />
       <ProgressPie />
     </>
   );
